bcc0136285c719721b0428670ef8984a4070c17f
Seth Schneider <schneids14@up.edu>
2014-10-09 00:37:13 +0000

    created schenids14 function Seth's Favorite food

---
+++ schneids14.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "schneids14.h"
+
+void username(){
+  printf("schneids14 like metals with lots of nickel and copper.\n");
+}
---
+++ schneids14.h
@@ -0,0 +1,2 @@
+/* this is file schenids14 */
+extern void schneids14();
